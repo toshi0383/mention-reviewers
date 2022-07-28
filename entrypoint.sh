cc0354633586ffa112fd09086dc84e07517beca0
Toshihiro Suzuki <t.suzuki326@gmail.com>
2022-07-28 12:44:33 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 mentioned_users=$(cat $GITHUB_EVENT_PATH | jq '.pull_request.requested_reviewers | map("@" + .login) | join(" ")')
 echo "Mentioned users: $mentioned_users"
 
-echo ::set-output name=mentions::$mentioned_users
+echo "a=b" >> $GITHUB_ENV
+echo "mentioned_names_string=$mentioned_users" >> $GITHUB_ENV
